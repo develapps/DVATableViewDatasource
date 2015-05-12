68c5c275a7d8fb3a27fbaa8ebf675b62bf7889b3
Pablo Romeu <pablo.romeu@develapps.es>
2015-05-12 17:32:46 +0200

    - Added some doc

--- Pod/Classes/NSArray+DVAViewModelDatasource.h
+++ Pod/Classes/NSArray+DVAViewModelDatasource.h
@@ -8,7 +8,13 @@
 
 #import <Foundation/Foundation.h>
 #import "DVAViewModelDataSourceProtocol.h"
-
+/**
+ @author Pablo Romeu, 15-05-12 17:05:44
+ 
+ Helper to use NSArray as DVAViewModelDataSourceProtocol
+ 
+ @since 1.1.0
+ */
 @interface NSArray (DVAViewModelDatasource) <DVAViewModelDataSourceProtocol>
 
 @end
--- Pod/Classes/NSDictionary+DVAViewModelDatasource.h
+++ Pod/Classes/NSDictionary+DVAViewModelDatasource.h
@@ -9,6 +9,13 @@
 #import <Foundation/Foundation.h>
 #import "DVAViewModelDataSourceProtocol.h"
 
+/**
+ @author Pablo Romeu, 15-05-12 17:05:30
+ 
+ Helper to use dictionaries as DVAViewModelDataSourceProtocol
+ 
+ @since 1.1.0
+ */
 @interface NSDictionary (DVAViewModelDatasource) <DVAViewModelDataSourceProtocol>
 
 @end
