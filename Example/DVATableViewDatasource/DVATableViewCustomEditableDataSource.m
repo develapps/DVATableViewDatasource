0fbabe2c5574765a1883ad5de78e5f6d52e716db
Pablo Romeu <pablo.romeu@develapps.com>
2015-11-24 15:22:25 +0100

    -  moved pods as subpods

--- Example/DVATableViewDatasource/DVATableViewCustomEditableDataSource.m
+++ Example/DVATableViewDatasource/DVATableViewCustomEditableDataSource.m
@@ -65,6 +65,12 @@
         [[self.viewModelArray objectAtIndex:indexPath.section] count]==0) {
         return nil;
     }
+    if (indexPath.section == [self.viewModelArray count]-1) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self tappedButton];
+        });
+
+    }
 
     return [[self viewModelsAtSection:indexPath.section] objectAtIndex:indexPath.row];
 }
@@ -136,6 +142,19 @@
                                                        inSection:0];
         
             NSLog(@"moving %@ to %@",ip,ipOther);
+        
+        
+        NSUInteger section = [self.viewModelArray count];
+            array = [NSMutableArray arrayWithObject:cellVM];
+            [self.manager dva_animateInsertSectionViewModel:array atIndexPath:[NSIndexPath indexPathForItem:0 inSection:section]];
+            // Insert
+            cellVM.title = @"INSERTED CELL";
+            [self.manager dva_animateInsertViewModel:cellVM atIndexPath:[NSIndexPath indexPathForItem:0 inSection:section]];
+            [self.manager dva_animateInsertViewModel:cellVM atIndexPath:[NSIndexPath indexPathForItem:1 inSection:section]];
+            [self.manager dva_animateInsertViewModel:cellVM atIndexPath:[NSIndexPath indexPathForItem:2 inSection:section]];
+            [self.manager dva_animateInsertViewModel:cellVM atIndexPath:[NSIndexPath indexPathForItem:3 inSection:section]];
+
+        
             [self.manager dva_animateMoveViewModelAtIndexPath:ip toIndexPath:ipOther];
             
             
